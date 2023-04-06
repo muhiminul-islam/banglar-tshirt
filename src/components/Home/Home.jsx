f0665de198fbb8154c7892e14ddd8a2130da6c23
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-07 00:54:43 +0600

    data shows dynamically and initially event handler btn added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Tshirt from '../Tshirt/Tshirt';
+import './Home.css';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        console.log(tshirt);
+    }
+
     return (
-        <div>
-            <h2>Total tShirts: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirt-container'>
+            {
+                tshirts.map(tshirt => <Tshirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                handleAddToCart={handleAddToCart}
+                ></Tshirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
