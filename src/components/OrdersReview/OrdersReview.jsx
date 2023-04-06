3bcbbd99e58453efcb84504acf6357be49da1490
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-07 00:15:12 +0600

    first commit

---
+++ src/components/OrdersReview/OrdersReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersReview = () => {
+    return (
+        <div>
+            <h2>This is order Review Page</h2>
+        </div>
+    );
+};
+
+export default OrdersReview;
