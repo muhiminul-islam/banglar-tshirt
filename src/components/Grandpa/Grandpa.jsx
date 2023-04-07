f7cb3ba6f25974558d22d21ae3eda13b0212513b
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-07 14:57:29 +0600

    Context API instead of props drilling

--- src/components/Grandpa/Grandpa.jsx
+++ src/components/Grandpa/Grandpa.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import Aunty from '../Aunty/Aunty';
 import './Grandpa.css'
 
+export const RingContext = createContext('gold');
+export const MoneyContext = createContext(0);
+
 const Grandpa = () => {
+    const [money, setMoney] = useState(0);
     const ring = 'diamond';
     return (
         <div className='grandpa'>
             <h2>Grandpa</h2>
-            <section className='flex'>
-                <Father ring={ring}></Father>
-                <Uncle></Uncle>
-                <Aunty ring={ring}></Aunty>
-            </section>
+            <p>Has Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <RingContext.Provider value='golden Ring'>
+                    <section className='flex'>
+                        <Father ring={ring}></Father>
+                        <Uncle></Uncle>
+                        <Aunty ring={ring}></Aunty>
+                    </section>
+                </RingContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
